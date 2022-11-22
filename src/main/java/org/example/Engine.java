71b70dd9296d67b78fed771472da800845c43766
CecilieSJ <ceci749q@stud.kea.dk>
2022-11-22 10:42:25 +0100

    Switch case is started

--- src/main/java/org/example/Engine.java
+++ src/main/java/org/example/Engine.java
@@ -1,19 +1,38 @@
 package org.example;
+
 import java.util.Scanner;
 
 public class Engine {
     Scanner scan = new Scanner(System.in);
-    public void startMenu(){
+
+    public void startMenu() {
+      /*  boolean isRunning = true;
+        while (isRunning){
+            String command = scan.nextLine();
+
+            switch (command) {
+                case "1" -> addMember();
+                case "2" ->
+                case "3" ->
+                case "4" ->
+                case "5" ->
+                case "6" ->
+                case "7" ->
+                case "8" ->
+                case "9" ->
+                case "0" ->
+
+            }*/
         int menu;
         menu = scan.nextInt();
         scan.nextLine();
-        if (menu == 1){
+        if (menu == 1) {
             addMember();
         }
     }
 
     private void addMember() {
-        try{
+        try {
             System.out.println();
             String surName = scan.next();
             scan.nextLine();
@@ -22,7 +41,7 @@ public class Engine {
             int month = scan.nextInt();
             int year = scan.nextInt();
             boolean competitionSwimmer = scan.hasNextBoolean();
-            System.out.println(surName+lastName+date+month+year+competitionSwimmer);
+            System.out.println(surName + lastName + date + month + year + competitionSwimmer);
         } catch (Exception e) {
             System.out.println("There was a typo in the information");
         }
