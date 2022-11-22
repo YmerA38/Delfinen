463069f7ebb3ee722de1c45ce87da6d9010ce27c
CarlHarlang <carl856k@stud.kea.dk>
2022-11-22 10:29:05 +0100

    Began working on addMember method.

--- src/main/java/org/example/Engine.java
+++ src/main/java/org/example/Engine.java
@@ -1,4 +1,31 @@
 package org.example;
+import java.util.Scanner;
 
 public class Engine {
+    Scanner scan = new Scanner(System.in);
+    public void startMenu(){
+        int menu;
+        menu = scan.nextInt();
+        scan.nextLine();
+        if (menu == 1){
+            addMember();
+        }
+    }
+
+    private void addMember() {
+        try{
+            System.out.println();
+            String surName = scan.next();
+            scan.nextLine();
+            String lastName = scan.next();
+            int date = scan.nextInt();
+            int month = scan.nextInt();
+            int year = scan.nextInt();
+            boolean competitionSwimmer = scan.hasNextBoolean();
+            System.out.println(surName+lastName+date+month+year+competitionSwimmer);
+        } catch (Exception e) {
+            System.out.println("There was a typo in the information");
+        }
+    }
+
 }
