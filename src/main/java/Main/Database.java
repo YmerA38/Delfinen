4307b01153fc2a4d6c73a912175892665b5bd869
YmerA38 <stin166c@stud.kea.dk>
2022-11-28 22:47:56 +0100

    Brugernavn:Stinus Kodeord: 1234

--- src/main/java/Main/Database.java
+++ src/main/java/Main/Database.java
@@ -13,7 +13,7 @@ public class Database {
 
     public Database(){
         memberList = new ArrayList<>();
-        addMember(new Member("Stinus","Helweg Andersen", LocalDate.of(1975,1,21),true,true,true,LocalDate.now(),1,Team.SENIOR_COMPETE,"Stinus","1234",Users.CHAIRMAN));  // TODO midlertidig
+        memberList.add(new Member("Stinus","Helweg Andersen", LocalDate.of(1975,1,21),true,true,true,LocalDate.now(),2,Team.SENIOR_COMPETE,"Stinus","1234",Users.CHAIRMAN));  // TODO midlertidig
 
     }
 
